69ab6ea010f5eac130ac47197b7163b10fd6d897
TheComet <alex.murray@gmx.ch>
2025-12-19 01:37:35 +0100

    Nicer colors, undo state sharing optimization because it was wrong

--- iir.c
+++ iir.c
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 
 /*
- *        b0 + b1*s + b2*s^2
- * T(s) = ------------------
- *        1 - a1*s - a2*s^2
+ *        b0 + b1*z^-1 + b2*z^-2
+ * H(z) = ----------------------
+ *        1 - a1*z^-1 - a2*z^-2
  */
 struct biquad_coeffs {
     int16_t b0, b1, b2;
@@ -21,60 +21,73 @@ static const struct biquad_coeffs coeffs[] = {
    { 16384, 32641, 16257, -23009, 12057 },
 };
 
-struct biquad_cascade {
-    int16_t history[3*2+2];
-    int16_t saved_fraction[3];
+struct biquad_state {
+    int16_t x1, x2;
+    int16_t y1, y2;
+    int16_t saved_fraction;
 };
 
-static int16_t filter(int16_t input, struct biquad_cascade* state, const struct biquad_coeffs* coeffs) {
+static int16_t filter(int16_t input, struct biquad_state* state, const struct biquad_coeffs* coeffs) {
     int i;
     int16_t output, frac;
 
     for (i = 0; i != 3; ++i) {
-        int32_t acc = state->saved_fraction[i];
+        int32_t acc = state[i].saved_fraction;
 
         acc += coeffs[i].b0 * input;
-        acc += coeffs[i].b1 * state->history[i*2+0];
-        acc += coeffs[i].b2 * state->history[i*2+1];
-        acc -= coeffs[i].a1 * state->history[i*2+2];
-        acc -= coeffs[i].a2 * state->history[i*2+3];
+        acc += coeffs[i].b1 * state[i].x1;
+        acc += coeffs[i].b2 * state[i].x2;
+        acc -= coeffs[i].a1 * state[i].y1;
+        acc -= coeffs[i].a2 * state[i].y2;
 
         output = (int16_t)(acc >= 0 ? (acc+K)>>Q : (acc-K)>>Q);
         frac = (int16_t)(acc - (output << Q));
 
-        state->history[i*2+1] = state->history[i*2+0];
-        state->history[i*2+0] = input;
-        state->saved_fraction[i] = frac;
+        state[i].x2 = state[i].x1;
+        state[i].x1 = input;
+        state[i].y2 = state[i].y1;
+        state[i].y1 = output;
+        state[i].saved_fraction = frac;
 
         input = output;
     }
-
-    state->history[i*2+1] = state->history[i*2+0];
-    state->history[i*2+0] = input;
 
     return output;
 }
 
 static void stopband_test(void) {
-    struct biquad_cascade cascade = {0};
+    struct biquad_state state[3] = {0};
     double t = 0.0;
     const double fs = 1000;
     const double f = 200;
     while (t < 0.3) {
         int16_t x = (int16_t)(cos(2*M_PI*f*t) * (1<<Q));
-        int16_t y = filter(x, &cascade, coeffs);
+        int16_t y = filter(x, state, coeffs);
         printf("%d,%d\n", x, y);
         t += 1/fs;
     }
 }
 
 static void impulse_response_test(void) {
-    struct biquad_cascade cascade = {0};
+    struct biquad_state state[3] = {0};
     double t = 0.0;
     const double fs = 1000;
     while (t < 0.3) {
         int16_t x = t == 0.0 ? (int16_t)(1<<Q) : 0;
-        int16_t y = filter(x, &cascade, coeffs);
+        int16_t y = filter(x, state, coeffs);
+        printf("%d,%d\n", x, y);
+        t += 1/fs;
+    }
+}
+
+static void passband_test(void) {
+    struct biquad_state state[3] = {0};
+    double t = 0.0;
+    const double fs = 1000;
+    const double f = 5;
+    while (t < 0.3) {
+        int16_t x = (int16_t)(cos(2*M_PI*f*t) * (1<<Q));
+        int16_t y = filter(x, state, coeffs);
         printf("%d,%d\n", x, y);
         t += 1/fs;
     }
@@ -89,6 +102,7 @@ int main(int argc, char* argv[]) {
     switch (atoi(argv[1])) {
         case 1: stopband_test(); break;
         case 2: impulse_response_test(); break;
+        case 3: passband_test(); break;
         default: fprintf(stderr, "Unknown test index: %s\n", argv[1]); return EXIT_FAILURE;
     }
 
--- plot.py
+++ plot.py
@@ -12,8 +12,19 @@ pairs = (
 x, y = map(np.array, zip(*pairs))
 t = np.linspace(0, 0.3, len(x))
 
-plt.plot(t, x)
-plt.scatter(t, x)
-plt.plot(t, y)
-plt.scatter(t, y)
+color_table = (
+    ("#FF6060", "#FF8080", "#C02020"),
+    ("#60FFFF", "#A0FFFF", "#20C0C0"),
+    ("#FF8000", "#FFA000", "#C07010"),
+    ("#FF60FF", "#FFA0FF", "#C020C0"),
+    ("#6060FF", "#A0A0FF", "#2020C0"),
+    ("#60FF60", "#A0FFA0", "#20C020"),
+)
+
+plt.plot(t, x, color=color_table[0][0])
+plt.scatter(t, x, color=color_table[0][1])
+plt.plot(t, y, color=color_table[1][0])
+plt.scatter(t, y, color=color_table[1][1])
+plt.gca().set_facecolor("#011627")
+plt.grid(visible=True)
 plt.show()
